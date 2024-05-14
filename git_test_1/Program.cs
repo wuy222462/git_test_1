f12796735e68b7450e9096f396fda0b7f6a0ac25
Tom <wuy222462@gmail.com>
2024-05-15 01:04:22 +0800

    third commit

--- git_test_1/Program.cs
+++ git_test_1/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Hello git");
 Console.WriteLine("Hello git");
 Console.WriteLine("Hello git22222222");
 Console.WriteLine(GetSum());
-
+Console.WriteLine("Hello !!!!!!!!!!");
 
 static int GetSum()
 {	
