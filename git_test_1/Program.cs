2eb2db7b383c78a85e56a88a36dcdff48d1c144a
Tom <wuy222462@gmail.com>
2024-05-15 01:02:09 +0800

    second commit

--- git_test_1/Program.cs
+++ git_test_1/Program.cs
@@ -1,7 +1,11 @@
 ﻿
 Console.WriteLine("Hello git");
 Console.WriteLine("Hello git");
+Console.WriteLine("Hello git");
+Console.WriteLine("Hello git");
+Console.WriteLine("Hello git22222222");
 Console.WriteLine(GetSum());
+
 
 static int GetSum()
 {	
